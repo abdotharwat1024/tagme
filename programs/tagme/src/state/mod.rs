1528d70524eff78284ea92649cbc76629519cff2
Abdo Ammar <Abdelrahman.TharwatAmmar@dekimo.com>
2025-12-24 17:44:41 +0100

    remove program_state.rs as it doesn't has any effect

--- programs/tagme/src/state/mod.rs
+++ programs/tagme/src/state/mod.rs
@@ -1,12 +1,10 @@
 mod ownership_history;
 mod prod_reg_acc;
-mod program_state;
 mod status;
 mod user;
 
 pub use ownership_history::*;
 pub use prod_reg_acc::*;
-pub use program_state::*;
 pub use status::*;
 pub use user::*;
 
--- programs/tagme/src/state/program_state.rs
+++
@@ -1,6 +0,0 @@
-use anchor_lang::prelude::Pubkey;
-
-pub struct ProgramState {
-    admin_pubkey: Pubkey,
-    version: u8,
-}
